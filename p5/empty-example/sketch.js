9223d0d592f699a8f3f3b46b9e56baa1dc257797
sujineering <98440320+sujineering@users.noreply.github.com>
2023-11-25 21:53:59 -0500

    Add files via upload

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,86 @@
+
+
+function setup() {
+  // put setup code here
+  createCanvas(1230,590); }
+
+function draw() {
+  
+  // put drawing code here
+
+  
+  fill("#8a6240");
+  rect(820,450,20,130);
+   // tree //
+
+
+
+
+fill("#034f1b");
+noStroke();
+  triangle(825,260,750,380,900,380);
+  triangle(825,360,750,480,900,480);
+
+  fill("#576CA8")
+  ellipse(810, 350, 10, 10)
+   fill("orange")
+  ellipse(850, 340, 10, 10)
+  fill(255,0,0)
+  ellipse(840, 420, 10, 10)
+   fill("orange")
+   ellipse(810, 430, 10, 10)
+  fill(255,0,0)
+   ellipse(805, 460, 10, 10)
+   fill(0,0,255)
+    ellipse(850, 460, 10, 10)
+  fill(255,0,0)
+   ellipse(820, 400, 10, 10)
+  fill(255,0,0)
+  ellipse(830, 300, 10, 10)
+
+
+  fill("yellow");
+  noStroke();
+    triangle(800,270,825,230,850,270);
+    fill("yellow");
+  noStroke();
+    triangle(800,240,850,240,825,285);
+
+
+  fill("white");
+  ellipse(400,340,130,130);
+  ellipse(400,460,160,160);
+
+
+fill("black");
+  ellipse(380,340,18,18);
+  ellipse(430,340,18,18);
+
+  fill("pink");
+  ellipse(405,380,18,18);
+
+
+  
+  fill("white")
+  rect(0,560,1231,50);
+
+
+
+fill("red")
+textSize(50); 
+textStyle(ITALIC);
+text("Merry Christmas ♡",width/3, height/4);
+  
+
+
+
+  
+    // console.log(i); 
+    fill(255, 255, 255, random(755));
+    ellipse(random(1230), random(590), random(3));
+    
+
+
+}
+  
+  
